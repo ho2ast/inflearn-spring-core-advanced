c6ed5ec9dcf1a3ecc5e6d127be74b713ab371234
HO2AST <lucid7@hotmail.co.kr>
2022-05-08 17:30:03 +0900

    first commit

---
+++ src/main/java/hello/proxy/app/v2/OrderRepositoryV2.java
@@ -0,0 +1,19 @@
+package hello.proxy.app.v2;
+
+public class OrderRepositoryV2 {
+  public void save(String itemId) {
+    //save logic
+    if (itemId.equals("ex")) {
+      throw new IllegalStateException("exception occured");
+    }
+    sleep(1000);
+  }
+
+  private void sleep(int millis) {
+    try {
+      Thread.sleep(millis);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+}
